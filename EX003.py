4961a6c154722329541e100188b5cf6591573efa
Jonathan Carvalho <125627147+aranhajs@users.noreply.github.com>
2023-08-21 23:26:44 -0300

    Add files via upload

---
+++ EX003.py
@@ -0,0 +1,6 @@
+#CRIE UM PROGRAMA QUE SOME DOIS NUMEROS:
+
+numero01= int(input('Digite um numero: '))
+numero02= int(input('Digite outro numero: '))
+result= numero01+numero02
+print(f'a soma entre {numero01} e {numero02}, é {result}' )
